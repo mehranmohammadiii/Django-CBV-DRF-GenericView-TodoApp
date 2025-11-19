f87800a9bc0eec938dff597812a886491c500546
mehranmohammadiii <mehranmohammaadiii@gmail.com>
2025-11-19 19:47:31 +0330

    Insert Data from Faker

--- todo/management/commands/insert_data.py
+++ todo/management/commands/insert_data.py
@@ -3,6 +3,7 @@ from faker import Faker
 from django.contrib.auth.models import User
 from todo.models import Task
 
+
 class Command(BaseCommand):
     help = "Insert Data to DataBase"
 
@@ -10,25 +11,23 @@ class Command(BaseCommand):
         super().__init__(*args, **kwargs)
         self.fake = Faker()
 
-
     def handle(self, *args, **options):
 
         user = User.objects.create(
-            username = self.fake.user_name(),
-            password = 'M32201513m',
-            email = self.fake.email(),
-            is_active = True,
-            first_name = self.fake.name(),
-            last_name = self.fake.last_name()
+            username=self.fake.user_name(),
+            password="M32201513m",
+            email=self.fake.email(),
+            is_active=True,
+            first_name=self.fake.name(),
+            last_name=self.fake.last_name(),
         )
         print(user)
 
-        self.stdout.write(self.style.SUCCESS('Successfully Create User'))
+        self.stdout.write(self.style.SUCCESS("Successfully Create User"))
 
         for i in range(5):
             task = Task.objects.create(
-                user = user,
-                title = self.fake.paragraph(nb_sentences=1)
+                user=user, title=self.fake.paragraph(nb_sentences=1)
             )
         print(task)
-        self.stdout.write(self.style.SUCCESS('Successfully Create Task for User'))
+        self.stdout.write(self.style.SUCCESS("Successfully Create Task for User"))
